6470e9d7fbd8a412a56c02a32f5fcd803cacb81f
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-01-10 16:41:01 +0200

    Configure the song service localy

--- src/services/car/car.service.local.js
+++ src/services/car/car.service.local.js
@@ -3,74 +3,71 @@ import { storageService } from '../async-storage.service'
 import { makeId } from '../util.service'
 import { userService } from '../user'
 
-const STORAGE_KEY = 'car'
+const STORAGE_KEY = 'song'
 
 export const carService = {
     query,
     getById,
-    save,
     remove,
-    addCarMsg
 }
 window.cs = carService
 
 
 async function query(filterBy = { txt: '' }) {
-    var cars = await storageService.query(STORAGE_KEY)
-    const { txt, minSpeed, maxPrice, sortField, sortDir } = filterBy
+    var songs = await storageService.query(STORAGE_KEY)
+    const { txt } = filterBy
 
     if (txt) {
         const regex = new RegExp(filterBy.txt, 'i')
-        cars = cars.filter(car => regex.test(car.vendor) || regex.test(car.description))
+        songs = songs.filter(song => regex.test(song.name) || regex.test(song.artist))
     }
     
-    cars = cars.map(({ _id, vendor, price, speed, owner }) => ({ _id, vendor, price, speed, owner }))
     return cars
 }
 
-function getById(carId) {
-    return storageService.get(STORAGE_KEY, carId)
+function getById(songId) {
+    return storageService.get(STORAGE_KEY, songId)
 }
 
-async function remove(carId) {
+async function remove(songId) {
     // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, carId)
+    await storageService.remove(STORAGE_KEY, songId)
 }
+//No need to save or to edit songs 
+// async function save(song) {
+//     var savedSong
+//     if (song._id) {
+//         const songToSave = {
+//             _id: car._id,
+//             price: car.price,
+//             speed: car.speed,
+//         }
+//         savedCar = await storageService.put(STORAGE_KEY, carToSave)
+//     } else {
+//         const carToSave = {
+//             vendor: car.vendor,
+//             price: car.price,
+//             speed: car.speed,
+//             // Later, owner is set by the backend
+//             owner: userService.getLoggedinUser(),
+//             msgs: []
+//         }
+//         savedCar = await storageService.post(STORAGE_KEY, carToSave)
+//     }
+//     return savedCar
+// }
 
-async function save(car) {
-    var savedCar
-    if (car._id) {
-        const carToSave = {
-            _id: car._id,
-            price: car.price,
-            speed: car.speed,
-        }
-        savedCar = await storageService.put(STORAGE_KEY, carToSave)
-    } else {
-        const carToSave = {
-            vendor: car.vendor,
-            price: car.price,
-            speed: car.speed,
-            // Later, owner is set by the backend
-            owner: userService.getLoggedinUser(),
-            msgs: []
-        }
-        savedCar = await storageService.post(STORAGE_KEY, carToSave)
-    }
-    return savedCar
-}
+// async function addCarMsg(carId, txt) {
+//     // Later, this is all done by the backend
+//     const car = await getById(carId)
 
-async function addCarMsg(carId, txt) {
-    // Later, this is all done by the backend
-    const car = await getById(carId)
+//     const msg = {
+//         id: makeId(),
+//         by: userService.getLoggedinUser(),
+//         txt
+//     }
+//     car.msgs.push(msg)
+//     await storageService.put(STORAGE_KEY, car)
 
-    const msg = {
-        id: makeId(),
-        by: userService.getLoggedinUser(),
-        txt
-    }
-    car.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, car)
-
-    return msg
-}
+//     return msg
+// }
