9613c72e7d1580362c837a34e39c24946547eb84
NoaLeshem <noalesh20@gmail.com>
2025-01-14 14:51:02 +0200

    brought back code that was removed in commit f36588e. loadStations should be called from actions file...

--- src/pages/StationIndex.jsx
+++ src/pages/StationIndex.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service";
 import { stationService } from "../services/station";
 import { userService } from "../services/user";
 
+import { loadStations, removeStation } from '../store/actions/station.actions.js'
 import { StationList } from "../cmps/StationList";
 import { SideBar } from "../cmps/SideBar";
 import { AppHeader } from "../cmps/AppHeader";
 import { AppFooter } from "../cmps/AppFooter";
 
 export function StationIndex() {
+
   const [filterBy, setFilterBy] = useState(stationService.getDefaultFilter());
   const stations = useSelector(
     (storeState) => storeState.stationModule.stations
@@ -19,34 +22,37 @@ export function StationIndex() {
     loadStations(filterBy);
   }, [filterBy]);
 
-  async function loadStations(filterBy) {
+  
+  async function onRemoveStation(stationId) {
     try {
-      await stationService.query(filterBy);
+      await stationService.remove(stationId);
+      showSuccessMsg("Station removed");
+      loadStations(filterBy);
     } catch (err) {
-      console.error("Cannot load stations", err);
+      showErrorMsg("Cannot remove station");
     }
   }
 
   async function onAddStation() {
     try {
-      const station = { name: "New Station", songs: [] };
-      await stationService.save(station);
+      const station = { name: "New Station", songs: [] }; 
+      const savedStation = await stationService.save(station);
+      showSuccessMsg(`Station added (id: ${savedStation._id})`);
       loadStations(filterBy);
     } catch (err) {
-      console.error("Cannot add station", err);
+      showErrorMsg("Cannot add station");
     }
   }
 
   return (
     <main className="station-index">
-      <header>
+      <AppHeader /> 
+      <SideBar />
+      
         <h2>Stations</h2>
         {userService.getLoggedinUser() && (
           <button onClick={onAddStation}>New Station</button>
         )}
-      </header>
-      <AppHeader /> 
-      <SideBar />
       <StationList stations={stations} />
       <AppFooter />
 
