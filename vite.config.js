9ba704db9316a7d3a5236b40064fc3467b264023
ShayRabin <135171468+ShayRabin@users.noreply.github.com>
2025-01-16 11:22:35 +0200

    Added alias for imports

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: '../backend/public',
-		emptyOutDir: true,
-	},
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "../backend/public",
+    emptyOutDir: true,
+  },
+});
