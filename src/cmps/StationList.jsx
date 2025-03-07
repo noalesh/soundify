4556f8a64ea3390dd31b23d34024aaa1d62ca7cf
NoaLeshem <noalesh20@gmail.com>
2025-03-07 18:55:01 +0200

    Added playlists to HomePage.

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,23 +1,26 @@
 import { userService } from '../services/user'
 import { StationPreview } from './StationPreview'
 import { Link } from "react-router-dom";
-                     
+
 
 export function StationList({ stations }) {
     
     //TODO: Add options to delete a station, create a new station ...?
 
     return <section>
-        <h1>Your shows</h1>
+       {/* <h1>Your shows</h1>  */}
+       {console.log(stations)}
         <div className="dev-comments">Development note - TODO - currently all stations of all users are being shown!</div>
-        <ul className="station-list">
+       <ul className="station-list">
             {stations.map(station =>
-                    <Link className="station-list-item-container" to={`/station/${station._id}`}>{station.name}
-                        <li key={station._id}>
+                <Link className="station-list-item-container" to={`/station/${station._id}`}>{station.name}
+                    <li key={station._id} >
                         <StationPreview station={station}/>
-                        </li>
-                    </Link>)
+                    </li>
+                </Link>)
             }
-        </ul>
+        </ul>  
+
+        
     </section>
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom"
-
+import { StationList } from "../cmps/StationList"
+import { stationService } from "../services/station/station.service.local";
 
 export function HomePage() {
+    const [filterBy, setFilterBy] = useState("");
+      const [stations, setStations] = useState([]);
+    
+      async function loadStations() {
+        try {
+            console.log("calling from HomePage")
+            const stationsToSet = await stationService.query();
+            setStations(stationsToSet);
+        } catch (err) {
+            console.error("Failed to load stations:", err);
+            }
+        }
+    
+      useEffect(() => {
+        loadStations();
+      }, []);
+    
     return (
         <div>
         <section>
-            <h1 className="dev-comments">TODO - UNDER CONSTRUCTION... <br></br>HomePage</h1>
+            <h6 className="dev-comments">TODO - UNDER CONSTRUCTION... <br></br>HomePage</h6>
+
+
+            <h3>Your Playlists</h3>
+            <StationList stations={stations}/>
         </section >
 
          <section>
