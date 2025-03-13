e552f608e8daa0fe60b10cdf104dc93a1b39c89e
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-03-13 09:20:55 +0200

    Smal fix of play button

--- src/cmps/PlayButton.jsx
+++ src/cmps/PlayButton.jsx
@@ -1,16 +1,4 @@
-// import { IconsSvg } from "./IconsSvg";
 
-
-// export function PlayButton({ isPlaying, onTogglePlay }) {
-//     return (
-//       <button className="play-pause-btn" onClick={onTogglePlay}>
-//         <img 
-//           src={isPlaying ? "src/assets/imgs/Soundify-files/Pause.svg" : "src/assets/imgs/Soundify-files/PlayButton.svg"} 
-//           alt={isPlaying ? "Pause" : "Play"} 
-//         />
-//       </button>
-//     );
-//   }
 
 import { IconsSvg } from "./IconsSvg";
 
