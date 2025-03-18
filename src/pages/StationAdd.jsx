7cbdfbb1787643921075a0244599319dffbd61bc
NoaLeshem <noalesh20@gmail.com>
2025-03-18 13:10:45 +0200

    added modal for edit details of new station.

--- src/pages/StationAdd.jsx
+++ src/pages/StationAdd.jsx
@@ -1,11 +1,30 @@
 
 export function StationAdd() {
     
-    return (
+    function addListenersDetailsModal () {
+        const open_station_edit = document.getElementById('open-station-edit')
+        const modal_details_container = document.getElementById('modal_details_container')
+        const close_station_edit = document.getElementById('save-station-edit')
     
+        if (open_station_edit) { 
+            open_station_edit.addEventListener('click', () => {
+            modal_details_container.classList.add('show')
+            })
+        }
+       
+    
+        if (close_station_edit) {
+            close_station_edit.addEventListener('click', () => {
+                modal_details_container.classList.remove('show')
+            })
+        } 
+    }
+   
 
-    <section className="station-add">   
+    return (
         
+    <section className="station-add">   
+    
         <h3 className="dev-comments">This is StationAdd page.</h3>
         <h3 className="dev-comments">TODO... UNDER CONSTRUCTION.</h3>  
         
@@ -18,10 +37,34 @@ export function StationAdd() {
                 <h1>new playlist</h1> 
                 <h5>username | {/*userService.getLoggedinUser()*/} aaa</h5>
             </div>
+           
         </div>
 
       <div className="grid-item-2">
+        <button id="open-station-edit">
+            Edit Details
+        </button>
       </div>
+
+      <div class="modal-details-container" id="modal-details-container">
+        <div class="modal-details">
+            <h3>Edit playlist details</h3>
+            <form>
+                <h4>This is a form... UNDER CONSTRUCTION</h4>
+            </form>
+            <button id="save-station-edit">
+                Save
+            </button>
+        </div>
+      </div>
+
+      {    addListenersDetailsModal()
+ }
       </section>
+
+
+
+
     )
+
 }
